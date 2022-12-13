72f439b8216bda4d48b617c974de86783ec45e95
Suryanshu Patnaik <er.suryanshupatnaik.4@gmail.com>
2022-12-13 11:16:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function alert_1(){
+    alert("Verification of Norton's Theorem using Independent source")
+}
+function alert_2(){
+    alert("Verification of Norton's Theorem using Dependent source")
+}
